33fecefd8130bbd4865166148a2fae5a7fa35993
Scott Ashton <scottmortonashton@gmail.com>
2022-07-01 16:22:40 -0600

    Better upgrade system

--- cli-scripts/post-install.mjs
+++ cli-scripts/post-install.mjs
@@ -1,5 +1,6 @@
 import { execaCommand } from "execa";
-
+import yesno from "yesno";
+import replaceInFile from "replace-in-file";
 import { globbySync } from "globby";
 import fs from "fs";
 import semver from "semver";
@@ -11,11 +12,28 @@ if (process.env["npm_config_argv"] && JSON.parse(process.env["npm_config_argv"])
   process.exit(0);
 }
 
+const workspacePackages = await (async function getWorkspacePackageNames() {
+  if (!getWorkspacePackageNames.cached) {
+    try {
+      const val = Object.keys(
+        JSON.parse((await execaCommand(`yarn workspaces info`)).stdout.split("\n").slice(1, -1).join(""))
+      );
+      getWorkspacePackageNames.cached = val;
+    } catch (e) {
+      throw new Error("Unable to get yarn workspaces info. The output format has likely changed...");
+    }
+  }
+
+  return getWorkspacePackageNames.cached;
+})();
+
+let errors = [];
+
 const files = globbySync(["**/*/package.json"], { gitignore: true });
 files.forEach((file) => {
   const pkgJson = JSON.parse(String(fs.readFileSync(file)));
   const pkgName = pkgJson.name;
-  if (pkgName.indexOf("@umbrella-corp/") === 0) {
+  if (isWorkspacePackage(pkgName)) {
     const deps = {
       dependencies: pkgJson.dependencies || {},
       devDependencies: pkgJson.devDependencies || {},
@@ -29,28 +47,42 @@ files.forEach((file) => {
       ..._.intersection(Object.keys(deps.devDependencies), Object.keys(deps.peerDependencies)),
     ];
     if (duplicates.length) {
-      console.error(
-        `\n\nError in ${pkgName}. The packages \`${duplicates.toString()}\` are declared more than once across \`dependencies\`, \`devDependencies\`, and \`peerDependencies\`. Please fix this issue in ${file}\n\n`
-      );
-      process.exit(1);
+      errors.push({
+        msg: `Error in ${pkgName}. The packages "${duplicates.toString()}" are declared more than once across "dependencies", "devDependencies", and "peerDependencies". Please fix this issue in ${file}`,
+      });
     }
 
     Object.keys(deps).forEach((depType) => {
       Object.entries(deps[depType]).forEach(([dep, version]) => {
         //Ensure no semver ranges. They are abomination
         if (!String(version[0]).match(/\d/) && version[0] !== "*" && version !== "*") {
-          console.error(
-            `Semver range (e.g. ^ or ~) detected in package.json of ${pkgName}: "${dep}": "${version}". This is prohibited in this monorepo in order to prevent version duplication.`
-          );
-          process.exit(1);
+          errors.push({
+            msg: `Semver range (e.g. ^ or ~) detected in package.json of ${pkgName}: "${dep}": "${version}". This is prohibited in this monorepo in order to prevent version duplication.`,
+          });
         }
 
         //Ensure local package dependencies have a version of "*"
-        if (dep.indexOf("@umbrella-corp/") === 0 && version !== "*") {
-          console.error(
-            `Local dependency in package.json ${pkgName} for package ${dep} must be "*", not "${version}". At project root, please run:\n\nnpx replace-in-files-cli --string='"${dep}": "${version}"' --replacement='"${dep}": "*"' '*/**' '!node_modules'`
-          );
-          process.exit(1);
+        if (isWorkspacePackage(dep) && version !== "*") {
+          errors.push({
+            recoveryPrompt: async () => {
+              const yes = await yesno({
+                question: `Dependency in ${pkgName} for local package "${dep}" must be "*", not "${version}". Would you like to upgrade from "${version}" to "*"? (y/n)`,
+                defaultValue: null,
+              });
+
+              if (yes) {
+                await replaceInFile({
+                  to: `"${dep}": "*"`,
+                  from: `"${dep}": "${version}"`,
+                  files: "**/*/package.json",
+                  ignore: ["node_modules"],
+                });
+                return true;
+              } else {
+                return false;
+              }
+            },
+          });
         }
 
         //Ensure versions are identical across all packages.
@@ -58,15 +90,26 @@ files.forEach((file) => {
         if (priorVersion && priorVersion.version !== version) {
           const higherVersion = semver.gt(priorVersion.version, version) ? priorVersion.version : version;
 
-          const safeDepName = dep.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+          errors.push({
+            recoveryPrompt: async () => {
+              const yes = await yesno({
+                question: `The packages ${pkgJson.name} and ${priorVersion.name} are on different versions of ${dep}: ${version} and ${priorVersion.version}. Would you like to upgrade ${dep} from "${priorVersion.version}" to "${higherVersion}"? (y/n)`,
+                defaultValue: null,
+              });
 
-          console.error(
-            `The packages ${pkgJson.name} and ${priorVersion.name} are on different versions of ${dep}: ${version} and ${priorVersion.version}.\n
-Please upgrade this package globally across the monorepo by running\n
-npx replace-in-files-cli --regex='"${safeDepName}": "[^"]+"' --replacement='"${dep}": "${higherVersion}"' '*/**/package.json' '!node_modules' && yarn install\n\n`
-          );
-
-          process.exit(1);
+              if (yes) {
+                await replaceInFile({
+                  to: `"${dep}": "${higherVersion}"`,
+                  from: `"${dep}": "${priorVersion.version}"`,
+                  files: "**/*/package.json",
+                  ignore: ["node_modules"],
+                });
+                return true;
+              } else {
+                return false;
+              }
+            },
+          });
         }
 
         ensureNoDifferentVersionsMap[dep] = {
@@ -80,12 +123,41 @@ npx replace-in-files-cli --regex='"${safeDepName}": "[^"]+"' --replacement='"${d
   }
 });
 
-async function getWorkspacePackageNames() {
-  try {
-    return Object.keys(
-      JSON.parse((await execaCommand(`yarn workspaces info`)).stdout.split("\n").slice(1, -1).join(""))
-    );
-  } catch (e) {
-    throw new Error("Unable to get yarn workspaces info. The output format has likely changed...");
+if (errors.length) {
+  errors = _.orderBy(errors, (a) => Number(!!a.recoveryPrompt), "desc");
+  let shouldThrow = false;
+  let shouldReinstall = false;
+  for (let i = 0; i < errors.length; i++) {
+    const { msg, recoveryPrompt } = errors[i];
+    if (msg) {
+      console.error("\n\n" + msg + "\n\n");
+    } else if (recoveryPrompt) {
+      const didRecover = await recoveryPrompt();
+      if (didRecover) {
+        shouldReinstall = true;
+      } else {
+        shouldThrow = true;
+      }
+    } else {
+      shouldThrow = true;
+    }
   }
+
+  if (shouldThrow) {
+    process.exit(1);
+  }
+
+  if (shouldReinstall) {
+    await execaCommand("yarn install", {
+      buffer: false,
+      shell: true,
+      stdout: process.stdout,
+      stderr: process.stderr,
+      stdin: process.stdin,
+    });
+  }
+}
+
+function isWorkspacePackage(pkgName) {
+  return workspacePackages.includes(pkgName);
 }
