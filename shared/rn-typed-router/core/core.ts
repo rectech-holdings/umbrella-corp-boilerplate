540c8328953d148c01e4fa922ce3c7824ef76791
Scott Ashton <scottmortonashton@gmail.com>
2022-07-20 14:16:20 -0600

    Tweak

--- shared/rn-typed-router/core/core.ts
+++ shared/rn-typed-router/core/core.ts
@@ -1,6 +1,6 @@
-export { createUrlGenerator } from "../src/implementations/path.js";
-export { createRouteDefinition } from "../src/implementations/routes.js";
-export { createPathsObject } from "../src/implementations/path.js";
-export { ParamTypes, ParamTypesClass } from "../src/types/params.js";
-export { $pathType, $routeDefType, UrlString, PathObj } from "../src/types/path.js";
-export { RouteDef, LeafRouteDef, StackRouteDef, TabRouteDef } from "../src/types/routes.js";
+export * from "../src/implementations/path.js";
+export * from "../src/implementations/routes.js";
+export * from "../src/implementations/path.js";
+export * from "../src/types/params.js";
+export * from "../src/types/path.js";
+export * from "../src/types/routes.js";
