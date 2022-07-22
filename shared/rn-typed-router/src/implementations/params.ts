02d54437bdb6a9deb5cc25aae419ac855b758c6f
Scott Ashton <scottmortonashton@gmail.com>
2022-07-22 12:12:39 -0600

    Clean and validate params

--- shared/rn-typed-router/src/implementations/params.ts
+++ shared/rn-typed-router/src/implementations/params.ts
@@ -8,6 +8,7 @@ export type $paramsType = typeof $params;
 
 //We have to use private symbol accessors so that we expose a small typescript surface area to the outside world
 //but can still access the data we need within this file. A bit weird but likely the best approach for DX.
+const type = Symbol("type");
 const validators = Symbol("validators");
 const enumOptions = Symbol("enumOptions");
 const defaultValue = Symbol("defaultValue");
@@ -18,13 +19,17 @@ export class ParamTypesClass<
   willBeRequired extends boolean = true,
   hasDefaultValue extends boolean = false,
 > {
-  [validators]: (((val: T) => string) | ((val: T) => Promise<string>))[] = [];
+  [type]: "string" | "number" | "enum";
+  [validators]: ((val: T) => string)[] = [];
   [enumOptions]: T[] = [];
   [defaultValue]: undefined | T = undefined;
   [isRequired]: boolean = true;
 
-  constructor(a?: { enumOptions?: T[] }) {
-    this[enumOptions] = a?.enumOptions || [];
+  constructor(a: { type: "string" } | { type: "number" } | { type: "enum"; enumOptions?: T[] }) {
+    this[type] = a.type;
+    if (a.type === "enum") {
+      this[enumOptions] = a?.enumOptions || [];
+    }
   }
 
   optional() {
@@ -38,7 +43,7 @@ export class ParamTypesClass<
     return this as any as ParamTypesClass<T, false, true>;
   }
 
-  addValidator(fn: ((val: T) => string) | ((val: T) => Promise<string>)) {
+  addValidator(fn: (val: T) => string) {
     this[validators].push(fn);
     return this;
   }
@@ -46,32 +51,74 @@ export class ParamTypesClass<
 
 export const ParamTypes = {
   string() {
-    return new ParamTypesClass<string>();
+    return new ParamTypesClass<string>({ type: "string" });
   },
 
   enum<F extends Record<PropertyKey, true>>(enumObj: F) {
-    return new ParamTypesClass<keyof F>({ enumOptions: Object.keys(enumObj) as any });
+    return new ParamTypesClass<keyof F>({ type: "enum", enumOptions: Object.keys(enumObj) as any });
   },
 
   number() {
-    return new ParamTypesClass<number>();
+    return new ParamTypesClass<number>({ type: "number" });
   },
 };
+
+function validateAndCleanParams(
+  params: Record<string, any>,
+  paramTypes: Record<string, ParamTypesClass<any, any, any>>,
+  validateType: "input" | "output",
+): { isValid: false; errors: string[] } | { isValid: true; params: Record<string, any> } {
+  const errors: string[] = [];
+  const finalParams = {};
+  Object.keys(paramTypes).forEach((k) => {
+    const typeVal = paramTypes[k]!;
+    const tp = typeVal[type];
+    const vldrs = typeVal[validators];
+    const eOpts = typeVal[enumOptions];
+    const dft = typeVal[defaultValue];
+    const isReq = typeVal[isRequired];
+
+    const val = validateType === "output" ? params[k] : params[k] ?? dft;
+
+    const matchesType = tp === "enum" ? !!eOpts.find((a) => a === val) : typeof val === tp;
+
+    if (isReq && !matchesType) {
+      errors.push(`Required param "${k}" not found.`);
+    }
+
+    if (!matchesType && !errors.length) {
+      errors.push(`Value "${val}" for param ${k} is not of type "${eOpts ? eOpts.join(",") : tp}"`);
+    }
+
+    if (val && vldrs.length) {
+      vldrs.forEach((fn) => {
+        const errorMsg = fn(val);
+        if (errorMsg) {
+          errors.push(errorMsg);
+        }
+      });
+    }
+
+    if (!errors.length && matchesType) {
+      Object.assign(finalParams, { [k]: val });
+    }
+  });
+
+  return errors.length ? { isValid: false, errors } : { isValid: true, params: finalParams };
+}
 
 export function validateAndCleanInputParams(
   inputParams: Record<string, any>,
   paramTypes: Record<string, ParamTypesClass<any, any, any>>,
 ): { isValid: false; errors: string[] } | { isValid: true; params: Record<string, any> } {
-  //TODO!!
-  return null as any;
+  return validateAndCleanParams(inputParams, paramTypes, "input");
 }
 
 export function validateAndCleanOutputParams(
   inputParams: Record<string, any>,
   paramTypes: Record<string, ParamTypesClass<any, any, any>>,
 ): { isValid: false; errors: string[] } | { isValid: true; params: Record<string, any> } {
-  //TODO!!
-  return null as any;
+  return validateAndCleanParams(inputParams, paramTypes, "output");
 }
 
 type InferType<T> = T extends ParamTypesClass<infer U, any, any> ? U : never;
