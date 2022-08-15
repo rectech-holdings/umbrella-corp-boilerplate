f5e9166c251a79593b27844656de81f2834f0b0d
Scott Ashton <scottmortonashton@gmail.com>
2022-08-15 14:13:33 -0600

    Expo web on monorepo

--- companies/acme/ac-app/src/App.tsx
+++ companies/acme/ac-app/src/App.tsx
@@ -1,21 +1,53 @@
+import { Suspense, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import Animated, { useSharedValue, useAnimatedStyle } from "react-native-reanimated";
+import { createApiReactSDK } from "ac-api";
+
+let Acme: Awaited<ReturnType<typeof createApiReactSDK>>;
+const AcmeProm = createApiReactSDK().then((a) => {
+  Acme = a;
+});
 
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <SafeAreaProvider style={{ flex: 1 }}>
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!!!</Text>
-          <StatusBar style="auto" />
-        </View>
-      </GestureHandlerRootView>
-    </SafeAreaProvider>
+    <Suspense>
+      <AppInner />
+    </Suspense>
   );
+}
+
+function AppInner() {
+  if (!Acme) {
+    throw AcmeProm;
+  }
+
+  return (
+    <Acme.SDKProvider>
+      <SafeAreaProvider style={{ flex: 1 }}>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!!!</Text>
+            <StatusBar style="auto" />
+            <SomeComponent />
+          </View>
+        </GestureHandlerRootView>
+      </SafeAreaProvider>
+    </Acme.SDKProvider>
+  );
+}
+
+function SomeComponent() {
+  useEffect(() => {
+    Acme.SDK.loans.getAllLoans({}).then((a) => {
+      console.log(a);
+    }, console.error);
+  }, []);
+
+  return null;
 }
 
 const styles = StyleSheet.create({
--- companies/acme/ac-app/webpack.config.js
+++ companies/acme/ac-app/webpack.config.js
@@ -1,9 +1,26 @@
 const createExpoWebpackConfigAsync = require("@expo/webpack-config");
+const findWorkspacePackages = require("@pnpm/find-workspace-packages").default;
+const findWorkspaceDir = require("@pnpm/find-workspace-dir").default;
+
+async function getWorkspaceInfo() {
+  const orig = console.error;
+  console.error = () => {}; //Prevent logging some annoying errors...
+  const workspaceDir = await findWorkspaceDir(process.cwd());
+  const packages = await findWorkspacePackages(workspaceDir);
+  console.error = orig;
+  return packages.map((a) => a.manifest.name);
+}
 
 module.exports = async function (env, argv) {
-  env.mode = "development";
-  env.devtool = "inline-source-map";
-  const config = await createExpoWebpackConfigAsync(env, argv);
+  const workspacePackages = await getWorkspaceInfo();
+
+  const config = await createExpoWebpackConfigAsync(
+    {
+      ...env,
+      babel: { dangerouslyAddModulePathsToTranspile: workspacePackages },
+    },
+    argv,
+  );
 
   // Customize the config before returning it.
   return config;
