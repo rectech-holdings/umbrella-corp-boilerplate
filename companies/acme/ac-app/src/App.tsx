0ecc582bdaa1b8532b3aaef4776349e7fb3f4428
Scott Ashton <scottmortonashton@gmail.com>
2022-08-17 15:29:46 -0600

    Tweak whitelabel examples

--- companies/acme/ac-app/src/App.tsx
+++ companies/acme/ac-app/src/App.tsx
@@ -1,4 +1,6 @@
 import { Suspense, useEffect, StrictMode } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Navigator } from "./Router.js";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { GestureHandlerRootView, BorderlessButton } from "react-native-gesture-handler";
@@ -10,8 +12,77 @@ const AcmeProm = createApiReactSDK().then((a) => {
   Acme = a;
 });
 
-import { StyleSheet, Text, View } from "react-native";
-import { Navigator } from "./Router.js";
+import {
+  createUseWhitelabelForm,
+  createWhitelabelComponent,
+  createWhitelabelComponentWithOptions,
+} from "react-whitelabel-form";
+
+type InputProps = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
+type SelectProps = React.DetailedHTMLProps<React.SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>;
+
+const useAcmeForm = createUseWhitelabelForm({
+  Select: createWhitelabelComponentWithOptions<{ label: string } & SelectProps, { label: string; key: string }>((p) => {
+    const { label, errors, onChangeValue, value, options, ...nativeProps } = p;
+    return (
+      <select
+        {...nativeProps}
+        value={p.options.find((o) => o.key === p.value)?.key}
+        onChange={(e) => {
+          p.onChangeValue(p.options.find((o) => o.key === e.target.value)?.value);
+          nativeProps?.onChange?.(e);
+        }}
+      >
+        {p.options.map((o) => (
+          <option key={o.key} value={o.key}>
+            {o.label}
+          </option>
+        ))}
+      </select>
+    );
+  }),
+  Input: createWhitelabelComponent<{ label: string }, string>((p) => {
+    const { onChangeValue, errors, onBlur, onFocus, value } = p;
+
+    return (
+      <div>
+        <input
+          value={value}
+          onBlur={onBlur}
+          onFocus={onFocus}
+          onChange={(e) => {
+            onChangeValue(e.target.value);
+          }}
+        />
+        {errors.length ? <div>{errors[0]}</div> : null}
+      </div>
+    );
+  }),
+});
+
+function SomeForm() {
+  const { Input, Select, store, useStoreValue } = useAcmeForm({
+    initState: { text: "", selectVal: { complexVal: 123 } },
+  });
+
+  return (
+    <form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        if (!store.validate()) {
+          return;
+        }
+
+        const data = store.get();
+        //Do something with the data...
+
+        store.reset();
+      }}
+    >
+      <Input required field={(s) => s.text} label="Cool Input" />
+    </form>
+  );
+}
 
 export default function App() {
   return (
